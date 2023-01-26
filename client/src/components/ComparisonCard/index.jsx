7ab0ec0e5db630d55d690cd364061244f2fdcc16
Seth Wooten <slwooten14@gmail.com>
2023-01-26 16:30:02 -0500

    removed logs from comparison card

--- client/src/components/ComparisonCard/index.jsx
+++ client/src/components/ComparisonCard/index.jsx
@@ -4,12 +4,6 @@ import './comparisonCard.css';
 
 const ComparisonCard = ({ home, homeLogo, away, awayLogo, gamesPlayed }) => {
 
-  console.log('home: ', home);
-  console.log('homeLogo: ', homeLogo);
-  console.log('away: ', away);
-  console.log('awayLogo: ', awayLogo);
-  console.log('gamesPlayed: ', gamesPlayed);
-
   // logos
   const homeSrc = homeLogo;
   const awaySrc = awayLogo;
