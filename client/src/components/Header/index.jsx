f50f3494d94ab82c70812e009a797a2549378554
Seth Wooten <slwooten14@gmail.com>
2023-01-27 15:49:56 -0500

    added logo and favicon, updated title

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './header.css';
+import logoSrc from '../../imgs/ou-logo-big.png';
 
 const Header = () => {
   return (
     <div className='header-div'>
+      <img src={logoSrc} alt="over/under logo" />
       <h1 className='title'>Over / Under</h1>
       <p className="description">Quick stats to help you decide whether you should take the Over or the Under</p>
     </div>
