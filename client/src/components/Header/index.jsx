6d76b6f99c71290fcb98c1d13c5c122aec2e296c
Seth Wooten <slwooten14@gmail.com>
2023-01-26 19:54:29 -0500

    media queries and responsiveness

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,8 +4,9 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className='header-div'>
       <h1 className='title'>Over / Under</h1>
+      <p className="description">Quick stats to help you decide whether you should take the Over or the Under</p>
     </div>
   );
 };
