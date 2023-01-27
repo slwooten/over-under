a412387aba219c407c139f895af2cd5895e427d9
Seth Wooten <slwooten14@gmail.com>
2023-01-27 18:44:58 -0500

    added animation to logo

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import logoSrc from '../../imgs/ou-logo-big.png';
 const Header = () => {
   return (
     <div className='header-div'>
-      <img src={logoSrc} alt="over/under logo" />
+      <img className='main-logo' src={logoSrc} alt="over/under logo" />
       <h1 className='title'>Over / Under</h1>
       <p className="description">Quick stats to help you decide whether you should take the Over or the Under.</p>
     </div>
