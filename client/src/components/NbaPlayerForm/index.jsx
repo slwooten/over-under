ff1a71b3df3becd1f26d39b5b1440fa83e5f81df
Seth Wooten <slwooten14@gmail.com>
2023-02-04 11:37:24 -0500

    added nba and nfl player form components

---
+++ client/src/components/NbaPlayerForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NbaPlayerForm = () => {
+  return (
+    <div>NbaPlayerForm</div>
+  );
+};
+
+export default NbaPlayerForm;
--- client/src/components/PlayerStats/index.jsx
+++ client/src/components/PlayerStats/index.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+// search form components
+import NbaPlayerForm from '../NbaPlayerForm';
+import NflPlayerForm from '../NflPlayerForm';
 
 const PlayerStats = () => {
+
+  const [league, setLeague] = useState('');
+
+  const handleChange = (event) => {
+    setLeague(event.target.value);
+  };
+
   return (
-    <div>PlayerStats</div>
+    <>
+      <FormControl color='success' sx={{ width: '120px', margin: '1rem' }}>
+        <InputLabel id="demo-simple-select-label">League</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={league}
+          label="League"
+          onChange={handleChange}
+        >
+          <MenuItem value={'NBA'}>NBA</MenuItem>
+          <MenuItem value={'NFL'}>NFL</MenuItem>
+        </Select>
+      </FormControl>
+      {league === 'NBA' ? (
+        <NbaPlayerForm />
+      ) : league === 'NFL' ? (
+        <NflPlayerForm />
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
