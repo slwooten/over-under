fa77b4dfa16a5b460ebcb3236ab24e2eda8c5c03
Seth Wooten <slwooten14@gmail.com>
2023-04-15 09:40:32 -0400

    logging player name that was searched on front end

--- client/src/components/NbaPlayerForm/index.jsx
+++ client/src/components/NbaPlayerForm/index.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { TextField, Button, CircularProgress, Box } from '@mui/material'
+
+import './nbaPlayerSearchForm.css';
 
 const NbaPlayerForm = () => {
+
+  const [formState, setFormState] = useState({ firstName: '', lastName: '' });
+
+  // handle change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  }
+
+  // handle form submit
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("You searched for:", formState)
+
+    // clear form
+    setFormState({
+      firstName: '',
+      lastName: '',
+    })
+  }
+
   return (
-    <div>NbaPlayerForm</div>
+    <div>
+      <form onSubmit={handleFormSubmit} className='nba-player-search-form'>
+        <div>
+          <TextField
+            name='firstName'
+            value={formState.firstName}
+            onChange={handleChange}
+            sx={{ margin: '1rem' }}
+            color='success'
+            id="outlined-basic"
+            label="First Name"
+            variant="outlined"
+          />
+          <TextField
+            name='lastName'
+            value={formState.lastName}
+            onChange={handleChange}
+            sx={{ margin: '1rem' }}
+            color='success'
+            id="outlined-basic"
+            label="Last Name"
+            variant="outlined"
+          />
+        </div>
+        <Button sx={{ maxWidth: '6rem', margin: '1rem' }} color='success' variant="contained" type='submit'>Search</Button>
+      </form>
+    </div>
   );
 };
 
